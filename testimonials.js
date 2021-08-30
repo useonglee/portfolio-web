11388e7282917ad59c75b104d5a30c6220e5d776
useonglee <useong0830@naver.com>
2021-08-30 15:35:59 +0900

    Set #14 changed slide width and add line breaks between code

--- testimonials.js
+++ testimonials.js
@@ -3,12 +3,11 @@
 // TODO: 슬라이드 기능 구현
 const slideList = document.querySelector(".slide__list");
 const slideContents = document.querySelectorAll(".slide__content");
-const slidePrevBtn = document.querySelector(".slide__btn-prev");
-const slideNextBtn = document.querySelector(".slide__btn-next");
+const slidePrevBtn = document.querySelector(".slide__btn.prev");
+const slideNextBtn = document.querySelector(".slide__btn.next");
 const slideLen = slideContents.length;
-const slideWidth = 400;
+const slideWidth = 800;
 const slideSpeed = 300;
-const startNum = 0;
 
 // TODO: 무한 캐루셀을 위해 앞, 뒤로 하나씩 더 추가한다.
 slideList.style.width = slideWidth * (slideLen + 2) + "px";
@@ -21,9 +20,9 @@ let clonedLast = lastChild.cloneNode(true);
 // TODO: 복제본을 앞, 뒤로 추가해준다.
 slideList.appendChild(clonedFirst);
 slideList.insertBefore(clonedLast, slideList.firstElementChild);
-slideList.style.transform =
-  "translate3d(-" + slideWidth * (startNum + 1) + "px, 0px, 0px)";
-let curIndex = startNum;
+slideList.style.transform = "translate3d(-" + slideWidth + "px, 0px, 0px)";
+
+let curIndex = 0;
 let curSlide = slideContents[curIndex];
 curSlide.classList.add("slide__active");
 
@@ -34,17 +33,20 @@ slideNextBtn.addEventListener("click", function () {
     slideList.style.transform =
       "translate3d(-" + slideWidth * (curIndex + 2) + "px, 0px, 0px)";
   }
+
   if (curIndex === slideLen - 1) {
     setTimeout(function () {
       slideList.style.transition = "0ms";
       slideList.style.transform =
         "translate3d(-" + slideWidth + "px, 0px, 0px)";
     }, slideSpeed);
+
     curIndex = -1;
   }
-  curSlide.classList.remove("slide_active");
+
+  curSlide.classList.remove("slide__active");
   curSlide = slideContents[++curIndex];
-  curSlide.classList.add("slide_active");
+  curSlide.classList.add("slide__active");
 });
 
 // TODO: Prev button
@@ -54,15 +56,18 @@ slidePrevBtn.addEventListener("click", function () {
     slideList.style.transform =
       "translate3d(-" + slideWidth * curIndex + "px, 0px, 0px)";
   }
+
   if (curIndex === 0) {
     setTimeout(function () {
       slideList.style.transition = "0ms";
       slideList.style.transform =
         "translate3d(-" + slideWidth * slideLen + "px, 0px, 0px)";
     }, slideSpeed);
+
     curIndex = slideLen;
   }
-  curSlide.classList.remove("slide_active");
+
+  curSlide.classList.remove("slide__active");
   curSlide = slideContents[--curIndex];
-  curSlide.classList.add("slide_active");
+  curSlide.classList.add("slide__active");
 });
