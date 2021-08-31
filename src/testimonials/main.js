38cd03718ef0796b8c375699a5fb275189146ecf
useonglee <useong0830@naver.com>
2021-09-01 00:02:24 +0900

    Add #18 modal active function

--- src/testimonials/main.js
+++ src/testimonials/main.js
@@ -26,8 +26,13 @@ let curIndex = 0;
 let curSlide = slideContents[curIndex];
 curSlide.classList.add("slide__active");
 
+const modals = document.querySelectorAll(`#modal`);
+const modalBtnAll = document.querySelectorAll(".modal__btn");
+let modalIndex = 1;
+let modalBtn = modalBtnAll[modalIndex];
+
 // TODO: Next button
-slideNextBtn.addEventListener("click", function () {
+slideNextBtn.addEventListener("click", () => {
   if (curIndex <= slideLen - 1) {
     slideList.style.transition = slideSpeed + "ms";
     slideList.style.transform =
@@ -35,7 +40,7 @@ slideNextBtn.addEventListener("click", function () {
   }
 
   if (curIndex === slideLen - 1) {
-    setTimeout(function () {
+    setTimeout(() => {
       slideList.style.transition = "0ms";
       slideList.style.transform =
         "translate3d(-" + slideWidth + "px, 0px, 0px)";
@@ -47,10 +52,13 @@ slideNextBtn.addEventListener("click", function () {
   curSlide.classList.remove("slide__active");
   curSlide = slideContents[++curIndex];
   curSlide.classList.add("slide__active");
+
+  modalBtn = modalBtnAll[++modalIndex];
+  modalActive(modalBtn);
 });
 
 // TODO: Prev button
-slidePrevBtn.addEventListener("click", function () {
+slidePrevBtn.addEventListener("click", () => {
   if (curIndex >= 0) {
     slideList.style.transition = slideSpeed + "ms";
     slideList.style.transform =
@@ -58,7 +66,7 @@ slidePrevBtn.addEventListener("click", function () {
   }
 
   if (curIndex === 0) {
-    setTimeout(function () {
+    setTimeout(() => {
       slideList.style.transition = "0ms";
       slideList.style.transform =
         "translate3d(-" + slideWidth * slideLen + "px, 0px, 0px)";
@@ -70,4 +78,48 @@ slidePrevBtn.addEventListener("click", function () {
   curSlide.classList.remove("slide__active");
   curSlide = slideContents[--curIndex];
   curSlide.classList.add("slide__active");
+
+  modalBtn = modalBtnAll[--modalIndex];
+  modalActive(modalBtn);
 });
+
+// TODO: Modal 활성화 함수
+function modalActive(modalBtn) {
+  modalBtn.addEventListener("click", (event) => {
+    const filter = event.target.dataset.filter;
+
+    if (filter === null) {
+      return;
+    }
+
+    modals.forEach((modal) => {
+      if (filter === modal.dataset.type) {
+        modal.style.display = "flex";
+
+        // TODO: 모달창 닫기
+        const closeBtn = document.querySelector("#modal__close-btn");
+        console.log(closeBtn);
+
+        closeBtn.addEventListener("click", () => {
+          modal.style.display = "none";
+        });
+
+        // TODO: 모달창 영역 외에 클릭할 경우 모달창 닫기
+        modal.addEventListener("click", (event) => {
+          if (event.target.classList.contains("modal__overlay")) {
+            modal.style.display = "none";
+          }
+        });
+
+        // TODO: ESC 눌렀을 경우 모달창 닫기
+        window.addEventListener("keyup", (event) => {
+          if (modal.style.display === "flex" && event.key === "Escape") {
+            modal.style.display = "none";
+          }
+        });
+      }
+    });
+  });
+}
+
+modalActive(modalBtn);
