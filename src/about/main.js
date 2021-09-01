9099ce810cc4064b6fe4514200578ed7bde32ef4
useonglee <useong0830@naver.com>
2021-09-01 23:56:15 +0900

    Add #21 intersectionObserver feature to about content

---
+++ src/about/main.js
@@ -0,0 +1,27 @@
+"use strict";
+
+const aboutObserverCallback = (entries) => {
+  entries.forEach((entry) => {
+    const { target } = entry;
+    if (entry.isIntersecting) {
+      target.classList.add("visible");
+      observer.unobserve(target);
+    } else {
+      target.classList.remove("visible");
+    }
+  });
+};
+
+const aboutObserverOptions = {
+  root: null,
+  rootMargin: "0px",
+  threshold: 0.9,
+};
+
+const aboutObserver = new IntersectionObserver(
+  aboutObserverCallback,
+  aboutObserverOptions
+);
+const aboutContainer = document.querySelector(".about__container");
+
+aboutObserver.observe(aboutContainer);
