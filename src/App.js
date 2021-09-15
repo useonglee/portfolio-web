b6a80dc1b657b7c4607768cd1f0b2867d53394d8
useonglee <useong0830@naver.com>
2021-09-15 14:31:16 +0900

    Add #28 Skills component and scheduling method

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Component from './core/component.js';
 import Darkmode from '../src/components/Darkmode/darkmode.js';
 import Home from '../src/components/Home/home.js'
 import About from '../src/components/About/about.js'
+import Skills from '../src/components/Skills/skills.js';
 
 export default class App extends Component {
   init() {
@@ -11,27 +12,30 @@ export default class App extends Component {
   template() {
     return `
       <div data-component="darkmode"></div>
-      <section data-component="home" id="home" class="snap"></section>
-      <section data-component="about" id="about" class="section snap"></section>
+      <section data-component="home" id="home"></section>
+      <section data-component="about" id="about" class="section"></section>
+      <section data-component="skills" id="skills" class="section"></section>
     `
   }
 
   mounted() {
-    const { aboutObserver } = this;
+    const { scheduling } = this;
     const $darkmode = this.$target.querySelector('[data-component="darkmode"]');
     const $home = this.$target.querySelector('[data-component="home"]');
     const $about = this.$target.querySelector('[data-component="about"]');
+    const $skills = this.$target.querySelector('[data-component="skills"]');
 
     new Darkmode($darkmode);
     new Home($home);
     new About($about, {
-      aboutObserver: aboutObserver.bind(this)
+      scheduling: scheduling.bind(this)
+    });
+    new Skills($skills, {
+      scheduling: scheduling.bind(this)
     });
   }
 
-  aboutObserver() {
-    setTimeout(() => {
-      this.intersectionObserver("#about__container", "about__visible", 0.3, null);
-    }, 100);
+  scheduling(callback, delay) {
+    setTimeout(callback, delay);
   }
 }
