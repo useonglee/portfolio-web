901ba45544d2f881981e52c904dc58249ab565df
useonglee <useong0830@naver.com>
2021-09-15 14:41:04 +0900

    Refactor #28 delete exsting files to structure components

--- src/about/main.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-import { intersectionObserver } from "../observerAPI.js";
-
-const aboutContainer = document.querySelector(".about__container");
-
-intersectionObserver(aboutContainer, "about__visible", 0.3, null);
--- src/contact/main.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-import { intersectionObserver } from "../observerAPI.js";
-
-const contactContainer = document.querySelector(".contact__container");
-
-intersectionObserver(contactContainer, "contact__visible", 0.3, null);
